1f8955134550f27c507585f17f963f830ed83c04
Alex Rodrigues <arodrigues@mapr.com>
2015-09-09 17:23:22 +0100

    adds bash script to invoke Mahout recommendation calculation command

---
+++ scripts/calc_recommendations.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export MAHOUT_HOME=/opt/mapr/mahout/mahout-0.10.0
+RECSYS_MPPU=1000
+RECSYS_MAXSIMPERITEM=100
+$MAHOUT_HOME/bin/mahout itemsimilarity  -i /user/mapr/ratings.csv -o /user/mapr/recsys_output -s SIMILARITY_LOGLIKELIHOOD --tempDir /user/mapr/temp_`date '+%s'` -mppu $RECSYS_MPPU -m $RECSYS_MAXSIMPERITEM -b true
